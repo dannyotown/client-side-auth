d0440bcd6c8cb2a6444711ba276b60b30f833b65
Dtown <dannyotown@gmail.com>
2019-10-31 20:33:28 -0500

    mvp!

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import "./App.css"
 import React from "react"
+import {Link, Route} from 'react-router-dom'
+import SignIn from './SignIn';
+import Account from "./Account";
+import ProtectedRoute from "./ProtectedRoute";
+import {getToken} from '../utils/api';
+import Logout from "./Logout";
 
 function App() {
+	const signedIn = getToken()
 	return (
 		<div className="wrapper">
-			<h1>Welcome</h1>
+			<nav>
+				<Link to='/'>Home</Link>
+				{!signedIn && <Link to='/signin'>Sign in</Link>}
+				{signedIn && <Link to="/account">My Account</Link>}
+				{signedIn && <Link to="/logout">Logout</Link>}
+			</nav>
+			<Route exact path='/signin' component={SignIn}/>
+			<ProtectedRoute exact path='/account' component={Account}/>
+			<ProtectedRoute exact path='/logout' component={Logout}/>
 		</div>
 	)
 }
